0b090eda049bf0808ec1182bdc95e7aa3566e519
mis-ethan <ethan.green@ochsinc.org>
2025-08-27 10:50:46 -0700

    Create PDFtoPPA.py

---
+++ PDFtoPPA.py
@@ -0,0 +1,11 @@
+#Read PDF invoice into a dataframe and then convert it into a csv PPA 
+
+#output dataframe with invoice data (invoice number, date, line items, subtotal, etc)
+def read_invoice(invoice_path)
+  #do the thing
+  return data
+
+#use dataframe to create ppa 
+def create_ppa(ppa_path)
+  #do the thing
+  return
